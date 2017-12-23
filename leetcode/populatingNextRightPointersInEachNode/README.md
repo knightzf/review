5e25c91cfba93294546a1cd869538a52e8ca6da8
knightzf <knightzf@gmail.com>
2017-12-23 16:58:07 -0500

    add

---
+++ leetcode/populatingNextRightPointersInEachNode/README.md
@@ -0,0 +1,3 @@
+First feel: traverse by layer, which will require extra stack or queue
+
+Then I realized the next point of parent node can be used.
---
+++ leetcode/populatingNextRightPointersInEachNode/main.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
+#include <climits>
+#include <utility>
+#include <bitset>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <memory>
+
+using namespace std;
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root)
+    {
+        impl(root, nullptr);
+    }
+
+    void impl(TreeLinkNode* node, TreeLinkNode* parent)
+    {
+        if(!node) return;
+
+        if(parent)
+        {
+            if(node == parent->right)
+            {
+                node->next = parent->next ? parent->next->left : nullptr;
+            }
+            else
+            {
+                node->next = parent->right;
+            }
+        }
+
+        if(node->right)
+        {
+            impl(node->right, node);
+        }
+
+        if(node->left)
+        {
+            impl(node->left, node);
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+    TreeLinkNode* a = new TreeLinkNode(1);
+    TreeLinkNode* b = new TreeLinkNode(2);
+    TreeLinkNode* c = new TreeLinkNode(3);
+    a->left = b;
+    a->right = c;
+
+    s.connect(a);
+}
