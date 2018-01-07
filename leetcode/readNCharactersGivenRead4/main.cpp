252a95ab27d1a9e1f83f72809bb2b40af171dba9
knightzf <knightzf@gmail.com>
2018-01-07 15:26:00 -0500

    up

--- leetcode/readNCharactersGivenRead4/main.cpp
+++ leetcode/readNCharactersGivenRead4/main.cpp
@@ -23,8 +23,44 @@ using namespace std;
 
 class Solution {
 public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
     int read(char *buf, int n) {
+        int count = 0;
+        while(count < n)
+        {
+            char temp[4];
+            int c = read4(temp);
 
+            if(c == 0) break;
+
+            if(count + c < n)
+            {
+                count += c;
+                memcpy(buf, temp, c);
+                buf += c;
+                continue;
+            }
+            else if(count + c == n)
+            {
+                count += c;
+                memcpy(buf, temp, c);
+                buf += c;
+                break;
+            }
+            else
+            {
+                memcpy(buf, temp, n - count);
+                buf += n - count;
+                count = n;
+                break;
+            }
+        }
+
+        return count;
     }
 };
 
