380f5a07041a0b42ed92f923d296ee02f285a14d
knightzf <knightzf@gmail.com>
2018-01-07 15:25:09 -0500

    add

---
+++ leetcode/readNCharactersGivenRead4/README.md
@@ -0,0 +1,3 @@
+Was unsure what the question was about until looked at discussion.
+
+Idea is basically use another buffer to read data and copy to the result.
---
+++ leetcode/readNCharactersGivenRead4/main.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
+#include <climits>
+#include <utility>
+#include <bitset>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int read(char *buf, int n) {
+
+    }
+};
+
+int main()
+{
+    Solution s;
+}
