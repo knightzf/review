b88d68ff706c0d98fb1881a902bbd9fcbe0a214b
knightzf <knightzf@gmail.com>
2018-01-07 21:35:43 -0500

    add

---
+++ leetcode/findPeakElement/README.md
@@ -0,0 +1,2 @@
+This is really stupid question.
+It only needs to find any peak not the global peak.
---
+++ leetcode/findPeakElement/main.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
+#include <climits>
+#include <utility>
+#include <bitset>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num)
+    {
+        int low = 0;
+        int high = num.size()-1;
+
+        while(low < high)
+        {
+            int mid1 = (low+high)/2;
+            int mid2 = mid1+1;
+            if(num[mid1] < num[mid2])
+                low = mid2;
+            else
+                high = mid1;
+        }
+        return low;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> a{1, 6, 2, 3, 4, 5};
+    std::cout<<s.findPeakElement(a)<<std::endl;
+}
