bf793858bde13a89828bf7f826de7bd9c2cca4cb
knightzf <knightzf@gmail.com>
2018-01-16 18:20:48 -0500

    add

---
+++ leetcode/pickRandomItemByProbabilityOfWeight/main.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
+#include <climits>
+#include <utility>
+#include <bitset>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <memory>
+#include <iterator>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+
+    }
+};
+
+int main() {
+    Solution s;
+}
---
+++ leetcode/wordSearch/recursionVer.cpp
@@ -0,0 +1,121 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
+#include <climits>
+#include <utility>
+#include <bitset>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <memory>
+#include <iterator>
+
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word)
+    {
+        if(board.empty() || board[0].empty())
+        {
+            return false;
+        }
+
+        int row = board.size();
+        int col = board[0].size();
+
+        typedef std::unordered_map<char, std::vector<std::pair<int,int>>> CharMap;
+
+        CharMap charMap;
+        for(int i = 0; i < board.size(); ++i)
+        {
+            for(int j = 0; j < board[i].size(); ++j)
+            {
+                char c = board[i][j];
+                if(charMap.find(c) == charMap.end())
+                {
+                    charMap[c] = std::vector<std::pair<int,int>>{std::make_pair(i, j)};
+                }
+                else
+                {
+                    charMap[c].push_back(std::make_pair(i, j));
+                }
+            }
+        }
+
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+
+        if(charMap.find(word[0]) != charMap.end())
+        {
+            for(const auto& p : charMap[word[0]])
+            {
+                visited[p.first][p.second] = true;
+                if(impl(board, row, col, visited, word, 1, p.first, p.second))
+                {
+                    return true;
+                }
+                visited[p.first][p.second] = false;
+            }
+        }
+
+        return false;
+    }
+
+    bool impl(vector<vector<char>>& board,
+              int row,
+              int col,
+              vector<vector<bool>>& visited,
+              const std::string& word,
+              int idx,
+              int i,
+              int j)
+    {
+        if(idx == word.size()) return true;
+
+        char c = word[idx];
+
+        if(i > 0 && board[i - 1][j] == c && visited[i - 1][j] == false)
+        {
+            visited[i - 1][j] = true;
+            if(impl(board, row, col, visited, word, idx + 1, i - 1, j)) return true;
+            visited[i - 1][j] = false;
+        }
+
+        if(i < row - 1 && board[i + 1][j] == c && visited[i + 1][j] == false)
+        {
+            visited[i + 1][j] = true;
+            if(impl(board, row, col, visited, word, idx + 1, i + 1, j)) return true;
+            visited[i + 1][j] = false;
+        }
+
+        if(j > 0 && board[i][j - 1] == c && visited[i][j - 1] == false)
+        {
+            visited[i][j - 1] = true;
+            if(impl(board, row, col, visited, word, idx + 1, i, j - 1)) return true;
+            visited[i][j - 1] = false;
+        }
+
+        if(j < col - 1 && board[i][j + 1] == c && visited[i][j + 1] == false)
+        {
+            visited[i][j + 1] = true;
+            if(impl(board, row, col, visited, word, idx + 1, i, j + 1)) return true;
+            visited[i][j + 1] = false;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+}
