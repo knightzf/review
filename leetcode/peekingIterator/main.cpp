18977302231151628cfad0150eff5bae7ca198d7
knightzf <knightzf@gmail.com>
2018-02-06 21:10:50 -0500

    add

---
+++ leetcode/peekingIterator/main.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <list>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
+#include <climits>
+#include <utility>
+#include <bitset>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <memory>
+#include <iterator>
+
+using namespace std;
+
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        Iterator t(*this);
+        return t.next();
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        return Iterator::next();
+	}
+
+	bool hasNext() const {
+        return Iterator::hasNext();
+	}
+};
+
+int main() {
+    //Solution s;
+}
