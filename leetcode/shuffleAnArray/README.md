b1662b6ddb5238ea5eca4e4cf73e29de93d36210
knightzf <knightzf@gmail.com>
2018-04-19 22:22:11 -0400

    add

---
+++ leetcode/shuffleAnArray/README.md
@@ -0,0 +1,1 @@
+My solution is pretty good in terms of using randomness in the permutation process.
---
+++ leetcode/shuffleAnArray/main.cpp
@@ -0,0 +1,33 @@
+#include "header.h"
+
+class Solution {
+private:
+    vector<int> v;
+public:
+    Solution(const vector<int>& nums) : v(nums) {
+
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return v;
+    }
+
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> t = v;
+        int n = t.size();
+        for(int i = 0; i < n; ++i)
+        {
+            int r = rand() % (n - i);
+            std::swap(t[i], t[r]);
+        }
+        return t;
+    }
+};
+
+
+int main()
+{
+    //Solution s;
+}
