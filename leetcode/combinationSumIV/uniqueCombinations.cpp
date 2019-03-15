4321339fa721d2264f82d16edcea4951b098702c
knightzf <knightzf@gmail.com>
2019-03-15 09:23:56 -0400

    add

--- leetcode/combinationSumIV/uniqueCombinations.cpp
+++ leetcode/combinationSumIV/uniqueCombinations.cpp
@@ -4,15 +4,16 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
-        //sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
         vector<vector<int>> dp(n, vector<int>(target + 1, 0));
         for(int i = 0; i < n; ++i) dp[i][0] = 1;
         for(int i = 1; i < target + 1; ++i) dp[0][i] = (i % nums[0] ? 0 : 1);
         for(int i = 1; i < n; ++i)
         {
-            for(int j = nums[i]; j < target + 1; ++j)
+            for(int j = 1; j < target + 1; ++j)
             {
-                dp[i][j] += dp[i - 1][j] + dp[i][j - nums[i]];            
+                dp[i][j] += dp[i - 1][j];
+                if(j >= nums[i]) dp[i][j] += dp[i][j - nums[i]];            
             }
         }
         for(auto& i : dp)
@@ -41,6 +42,6 @@ int main()
 {
     Solution s;
     //vector<int> a{1, 2, 3};
-    vector<int> a{3, 2, 1};
-    cout<<s.combinationSum4(a, 4)<<endl;
+    vector<int> a{2, 1};
+    cout<<s.combinationSum4(a, 5)<<endl;
 }
